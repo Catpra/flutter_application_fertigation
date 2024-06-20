f476d879c90035af66a26f896cee6c4f81f044b4
Catpra <fadhil.saeho@binus.ac.id>
2024-06-20 16:55:22 +0700

    testing with BLE

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/schedule_provider.dart';
 import '../providers/bluetooth_provider.dart';
+import '../models/schedule.dart';
 
 class HomePage extends StatelessWidget {
   @override
